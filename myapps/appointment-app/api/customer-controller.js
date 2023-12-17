4d9c0b3f95a4ff913019e449325ce06287fe115f
unknown <gmaheswaranmc@gmail.com>
2023-12-17 14:02:02 +0530

    car wash app

--- myapps/appointment-app/api/customer-controller.js
+++ myapps/appointment-app/api/customer-controller.js
@@ -1,19 +1,20 @@
 const { CustomerModel} = require('./models')
+const md5= require('md5')
 class CustomerController{
    register = async(request, response) => {
 /*
 POST http://localhost:8080/customer/register
     {
-        name: 'narmada',
-        mobile: '9991',
-        password: '4321',
-        location: 'Srirampura'
+        "name": "narmada",
+        "mobile": "9991",
+        "password": "4321",
+        "location": "Srirampura"
     }
 */
         let responseCode = 200;
         let responseBody = {};
 
-        const inputCustomer = request.body;
+        const inputCustomer = {...request.body, password: md5(request.body.password)};
         console.log(inputCustomer);
         
         try{
